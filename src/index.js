13f45fac6e00eecc13a3713c6633a7e092e55b3b
Bao Toan <20161385@student.hcmute.edu.vn>
2025-02-18 16:30:33 +0700

    test fail testcase

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello, Docker CI/CD!");
 });
 
+app.get("/health", (req, res) => {
+  res.send("Server is running!");
+});
+
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- test/index.test.js
+++ test/index.test.js
@@ -10,3 +10,8 @@ test("GET / should return Hello, Docker CI/CD!", async () => {
   const response = await request(server).get("/");
   expect(response.text).toBe("Hello, Docker CI/CD!");
 });
+
+test("GET /health should return Server is running!", async () => {
+  const response = await request(server).get("/health");
+  expect(response.text).toBe("Server is running!!!");
+});
